2788f822f788f5fbbab0bd22179cd17cd9974764
anshmishra4444 <118624420+anshmishra4444@users.noreply.github.com>
2022-11-19 22:35:50 +0530

    Add files via upload
    Snake Water Gun Game In Python

---
+++ Snake Water Gun Game.py
@@ -0,0 +1,39 @@
+import random
+def gameWin(comp, you):
+    if comp == you:
+        return None
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+    elif comp == 'g':
+        if you == 'w':
+            return True
+        elif you == 's':
+            return False
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+print(input("Comp Turn: Snake(s) Water(W) or Gun(g)?"))
+randNo = random.randint(1, 3)
+if randNo ==1:
+    comp='s'
+elif randNo ==2:
+    comp='w'
+elif randNo ==3:
+    comp ='g'
+you = input("Your Turn: Snake(s) Water(W) or Gun(g)?")
+a = gameWin(comp, you)
+
+print(f"computer chose {comp}")
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
